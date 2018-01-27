890d764b8fa16dd0d7399b24c7faff28c90ff7d3
Stephen Eyerly <stepheneyerly@gmail.com>
2018-01-27 11:25:48 -0500

    added in account

--- interface/routes/routes.js
+++ interface/routes/routes.js
@@ -3,6 +3,16 @@ var async = require('async');
 var vogels = require('vogels');
 var Joi = require('joi');
 var AWS = require('aws-sdk');
+vogels.AWS.config.loadFromPath('credentials.json');
+
+var Account = vogels.define('Account', {
+  hashKey : 'username',
+    
+  schema : {
+    email   : Joi.string().email(),
+    name    : Joi.string(),
+  }
+});
 
 /* GET home page. */
 var getHome = function(req, res, next) {
